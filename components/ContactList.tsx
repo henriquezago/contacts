54bdb91a09437c3f240920b7f2f8d4ed1092c21f
Henrique Zago <lobato.zago@gmail.com>
2023-02-12 20:51:58 -0300

    feat(home): adds name filter to listing

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -4,6 +4,10 @@ import { Grid } from "@nextui-org/react";
 import { Contact } from "../pages/api/contacts";
 import ContactCard from "./ContactCard";
 import ContactModal from "./ContactModal";
+import useFilteredContacts from "../hooks/useFilteredContacts";
+import NameFilter from "./NameFilter";
+
+import styles from "../styles/ContactList.module.scss";
 
 type ContactListProps = {
   contacts: Contact[];
@@ -12,42 +16,46 @@ type ContactListProps = {
 }
 
 export default function ContactList({ contacts, onEdit, onDelete }: ContactListProps) {
-  const [selectedContact, setSelectedContact] = useState(null as Contact);
+  const [selectedContact, setSelectedContact] = useState<Contact>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const openModal = useCallback(contact => {
+  const { filteredContacts, filter, setFilter } = useFilteredContacts(contacts);
+
+  const openModal = useCallback((contact) => {
     setSelectedContact(contact);
     setIsModalVisible(true);
   }, [setSelectedContact, setIsModalVisible]);
 
-  const saveContact = useCallback(contact => {
+  const saveContact = useCallback((contact) => {
     onEdit(contact);
     setIsModalVisible(false);
   }, [onEdit]);
 
-  const deleteContact = useCallback(contactId => {
+  const deleteContact = useCallback((contactId) => {
     onDelete(contactId);
     setIsModalVisible(false);
-  }, [])
+  }, [onDelete, setIsModalVisible]);
 
-  const contactElements = contacts
-    .map((contact) =>
-      <Grid xs={2} key={contact.id}>
-        <ContactCard contact={contact}
-                     onClick={openModal} />
-      </Grid>
-    );
+  const contactElements =
+    filteredContacts
+      .map((contact) =>
+        <Grid xs={6} key={contact.id}>
+          <ContactCard contact={contact}
+            onClick={openModal} />
+        </Grid>
+      );
 
   return (
-    <>
+    <div className={styles.wrapper}>
+      <NameFilter filter={filter} onChange={newFilter => setFilter(newFilter)} />
       <Grid.Container gap={2} justify="center">
         {contactElements}
       </Grid.Container>
       <ContactModal contact={selectedContact}
-                    isVisible={isModalVisible}
-                    onClose={() => setIsModalVisible(false)}
-                    onDelete={deleteContact}
-                    onSave={saveContact} />
-    </>
+        isVisible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        onDelete={deleteContact}
+        onSave={saveContact} />
+    </div>
   );
 }
