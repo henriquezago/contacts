d152e9ffa455d6f4ef2a1f6cb44933671c182194
Henrique Zago <lobato.zago@gmail.com>
2023-02-12 20:51:42 -0300

    component: NameFilter

---
+++ components/NameFilter.tsx
@@ -0,0 +1,32 @@
+import { Input } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+
+import useDebounce from "../hooks/useDebounce";
+
+type NameFilterProps = {
+  filter: string;
+  onChange: (name: string) => void;
+}
+
+function NameFilter({ filter, onChange }: NameFilterProps) {
+  const [name, setName] = useState<string>(filter);
+
+  const debouncedName = useDebounce(name, 300);
+
+  useEffect(() => {
+    onChange(debouncedName);
+  }, [debouncedName]);
+
+  return (
+    <Input
+      id="name-filter"
+      type="text"
+      placeholder="Filter by name"
+      value={name}
+      // size="lg"
+      onChange={e => setName(e.target.value)}
+    />
+  );
+}
+
+export default NameFilter;
