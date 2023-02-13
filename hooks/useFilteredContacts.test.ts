ecdad513a1964bcd3486fd7da38d735bad80db4d
Henrique Zago <lobato.zago@gmail.com>
2023-02-13 19:03:42 -0300

    chore: changes useFilteredContacts.test extension

--- hooks/useFilteredContacts.test.ts
+++ hooks/useFilteredContacts.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals';
-
 import { act, renderHook } from "@testing-library/react";
+
 import useFilteredContacts from "./useFilteredContacts";
 
 describe("useFilteredContacts", () => {
